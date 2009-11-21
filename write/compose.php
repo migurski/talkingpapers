233abec14ccb539f29f6bdc9ecdb8b709d0c98df
Michal Migurski <migurski@Philippe.local>
2009-11-21 00:32:26 -0800

    Drawing some primitive form fields

--- write/compose.php
+++ write/compose.php
@@ -42,6 +42,74 @@
     $pdf->addPage();
     
     annotate_pdf($pdf, 'http://github.com/migurski/talkingpapers/network');
+
+   /**
+    * Form fields, loosely based on HTML5 input element spec.,
+    * http://www.w3.org/TR/html5/forms.html#the-input-element
+    */
+    $fields = array(array('name' => 'Last', 'type' => 'text', 'size' => 30),
+                    array('name' => 'First', 'type' => 'text', 'size' => 30),
+                    array('name' => 'Date Of Birth', 'type' => 'date'),
+                    array('name' => 'Nationality', 'type' => 'text'));
+    
+    $y = 144;
+    
+    function draw_text_box(&$pdf, $x, $y, $size, $subtext=false)
+    {
+        // width, height, right, bottom
+        $c = 18;
+        $w = $size * $c;
+        $h = 18;
+        $r = $x + $w;
+        $b = $y + $h;
+
+        $pdf->setDrawColor(0x00);
+        $pdf->setFont('Helvetica', 'I', 6);
+
+        // outline
+        $pdf->setLineWidth(.5);
+        $pdf->rect($x, $y, $w, $h, 'D');
+        
+        // ticks
+        for($o = $x + $c; $o < $r; $o += $c)
+            $pdf->line($o, $b, $o, $b - 4);
+
+        // thick underline
+        $pdf->setLineWidth(1.5);
+        $pdf->line($x + .75, $b - .75, $r - .75, $b - .75);
+
+        // optional subtext
+        if($subtext)
+            $pdf->text($x + 2, $b + 6, $subtext);
+    }
+    
+    foreach($fields as $field)
+    {
+        $x = 36;
+        
+        $pdf->setFont('Helvetica', 'B', 12);
+        $pdf->text($x, $y, $field['name']);
+        
+        $y += 6;
+        
+        switch($field['type'])
+        {
+            case 'text':
+                $size = $field['size'] ? $field['size'] : 20;
+                draw_text_box($pdf, $x, $y, $size);
+                break;
+
+            case 'date':
+                $pdf->setFont('Helvetica', 'I', 6);
+                draw_text_box($pdf, $x, $y, 4, 'year');
+                draw_text_box($pdf, $x + 78, $y, 2, 'month');
+                draw_text_box($pdf, $x + 120, $y, 2, 'day');
+                break;
+        }
+        
+        $y += 48;
+    }
+    
     $pdf->output('out.pdf', 'F');
 
 ?>
