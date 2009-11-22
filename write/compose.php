cedc3ea885231c2a959db37eb1f622404033cf59
Michal Migurski <migurski@Philippe.local>
2009-11-22 10:59:21 -0800

    Added textarea field

--- write/compose.php
+++ write/compose.php
@@ -4,6 +4,7 @@
 
     require_once 'FPDF/fpdf.php';
     require_once 'HTTP/Request.php';
+    require_once 'Services/JSON.php';
 
     function annotate_pdf(&$pdf, $print_url)
     {
@@ -24,7 +25,6 @@
 
         $req = new HTTP_Request('http://chart.apis.google.com/chart?chs=264x264&cht=qr&chld=Q|0');
         $req->addQueryString('chl', $print_url);
-        echo $req->getURL()."\n";
         $res = $req->sendRequest();
         
         if(PEAR::isError($res))
@@ -50,9 +50,33 @@
     $fields = array(array('name' => 'Last', 'type' => 'text', 'size' => 30),
                     array('name' => 'First', 'type' => 'text', 'size' => 30),
                     array('name' => 'Date Of Birth', 'type' => 'date'),
-                    array('name' => 'Nationality', 'type' => 'text'));
+                    array('name' => 'Nationality', 'type' => 'text'),
+                    array('name' => 'Comments', 'type' => 'textarea'));
     
     $y = 144;
+    
+    function draw_area_box(&$pdf, $x, $y, $r, $b, $subtext=false)
+    {
+        $w = $r - $x;
+        $h = $b - $y;
+
+        $pdf->setDrawColor(0x00);
+        $pdf->setFont('Helvetica', 'I', 6);
+
+        // outline
+        $pdf->setLineWidth(.5);
+        $pdf->rect($x, $y, $w, $h, 'D');
+
+        // thick underline
+        $pdf->setLineWidth(1.5);
+        $pdf->line($x + .75, $b - .75, $r - .75, $b - .75);
+
+        // optional subtext
+        if($subtext)
+            $pdf->text($x + 2, $b + 6, $subtext);
+        
+        return array($x, $y, $r, $b);
+    }
     
     function draw_text_box(&$pdf, $x, $y, $size, $subtext=false)
     {
@@ -63,27 +87,21 @@
         $r = $x + $w;
         $b = $y + $h;
 
-        $pdf->setDrawColor(0x00);
-        $pdf->setFont('Helvetica', 'I', 6);
+        // container box
+        draw_area_box(&$pdf, $x, $y, $r, $b, $subtext);
 
-        // outline
+        $pdf->setDrawColor(0x00);
         $pdf->setLineWidth(.5);
-        $pdf->rect($x, $y, $w, $h, 'D');
+        $pdf->setFont('Helvetica', 'I', 6);
         
         // ticks
         for($o = $x + $c; $o < $r; $o += $c)
             $pdf->line($o, $b, $o, $b - 4);
 
-        // thick underline
-        $pdf->setLineWidth(1.5);
-        $pdf->line($x + .75, $b - .75, $r - .75, $b - .75);
-
-        // optional subtext
-        if($subtext)
-            $pdf->text($x + 2, $b + 6, $subtext);
+        return array($x, $y, $r, $b);
     }
     
-    foreach($fields as $field)
+    foreach($fields as $f => $field)
     {
         $x = 36;
         
@@ -96,20 +114,34 @@
         {
             case 'text':
                 $size = $field['size'] ? $field['size'] : 20;
-                draw_text_box($pdf, $x, $y, $size);
+                $bbox = draw_text_box($pdf, $x, $y, $size);
+                $fields[$f]['bbox'] = $bbox;
+                break;
+
+            case 'textarea':
+                $bbox = draw_area_box(&$pdf, $x, $y, $x + 540, $y + 72);
+                $fields[$f]['bbox'] = $bbox;
                 break;
 
             case 'date':
                 $pdf->setFont('Helvetica', 'I', 6);
-                draw_text_box($pdf, $x, $y, 4, 'year');
-                draw_text_box($pdf, $x + 78, $y, 2, 'month');
-                draw_text_box($pdf, $x + 120, $y, 2, 'day');
+                $bbox1 = draw_text_box($pdf, $x, $y, 4, 'year');
+                $bbox2 = draw_text_box($pdf, $x + 78, $y, 2, 'month');
+                $bbox3 = draw_text_box($pdf, $x + 120, $y, 2, 'day');
+                $fields[$f]['bbox'] = array(min($bbox1[0], $bbox2[0], $bbox3[0]),
+                                            min($bbox1[1], $bbox2[1], $bbox3[1]),
+                                            max($bbox1[2], $bbox2[2], $bbox3[2]),
+                                            max($bbox1[3], $bbox2[3], $bbox3[3]));
                 break;
         }
         
-        $y += 48;
+        $y = $fields[$f]['bbox'][3];
+        $y += 24;
     }
     
     $pdf->output('out.pdf', 'F');
+    
+    $json = new Services_JSON(SERVICES_JSON_LOOSE_TYPE);
+    echo $json->encode($fields)."\n";
 
 ?>
