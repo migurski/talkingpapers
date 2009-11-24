1e1ae721b17ea76e73cc91032e0d8ac0e1b8b038
Chris Blow <unthinkingly@gmail.com>
2009-11-24 02:47:31 -0800

    updated sass

---
+++ sinatra/app.rb
@@ -0,0 +1,24 @@
+#! /usr/bin/ruby
+require 'rubygems'
+require 'sinatra'
+
+get '/stylesheet.css' do
+  header 'Content-Type' => 'text/css; charset=utf-8'
+   sass :stylesheet, :sass_options => {:style => :expanded } # overridden
+end
+
+get '/' do
+  haml :index
+end
+
+post '/' do
+  # create a form
+end
+
+put '/' do
+  #update a form
+end
+
+delete '/' do
+  #delete a form
+end
